5af5fcf42b963d4999da448cc58b81e1d3500706
ajb17 <anthony.beetem@gmail.com>
2019-04-30 14:21:22 -0400

    The pipeline will run all the way through, and can handle subject wiht missing files without crashing, although there isn't much indication when this happens. The pipeline is still locked to the single mean template, and the two derivate templates given

--- cpac_epi_reg.py
+++ cpac_epi_reg.py
@@ -27,6 +27,7 @@ from bids import BIDSLayout
 
 #from pipeline_plus import get_transforms
 #import itertools
+from nipype import DataGrabber
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-s', '--starter', required=False, help='The image that is applied to the template to create transforms')
@@ -120,6 +121,19 @@ print_node = Node(Function(function=print_inputs), name='print_inputs')
 # testflow.run()
 
 
+data_grabber_node = Node(DataGrabber(base_directory=args['directory'], sort_filelist=True, raise_on_empty=False, infields=['sub', 'ses'], outfields=['mean', 'reho', 'res_filt']), name='data_grabber')
+data_grabber_node.inputs.template = '*'
+data_grabber_node.inputs.raise_on_empty = True
+data_grabber_node.inputs.drop_blank_outputs = True
+data_grabber_node.inputs.field_template = dict(mean="/projects/cgutierrez/rockland_sample_cpac/All_Outputs/%s_%s/mean_functional/_scan_rest_acq-1400/sub-%s_ses-%s_task-rest_acq-1400_bold_calc_resample_volreg_calc_tstat.nii.gz",
+                                               reho="/projects/cgutierrez/rockland_sample_cpac/All_Outputs/%s_%s/raw_reho_map/_scan_rest_acq-1400/_compcor_ncomponents_5_selector_pc10.linear1.wm0.global1.motion1.quadratic1.gm0.compcor1.csf0/_bandpass_freqs_0.01.10.0/ReHo.nii.gz",
+                                               res_filt="/projects/cgutierrez/rockland_sample_cpac/All_Outputs/%s_%s/alff_img/_scan_rest_acq-1400/_compcor_ncomponents_5_selector_pc10.linear1.wm0.global1.motion1.quadratic1.gm0.compcor1.csf0/_hp_0.01/_lp_0.1/residual_filtered_3dT.nii.gz")
+data_grabber_node.inputs.template_args = dict(mean=[['sub', 'ses', 'sub', 'ses']],
+                                              reho=[['sub', 'ses']],
+                                              res_filt=[['sub', 'ses']])
+data_grabber_node.iterables = [('sub', sub_list), ('ses', ses_list)]
+
+
 generate_transforms_node = Node(legacy.GenWarpFields(reference_image=template), name='generate_transforms')
 
 merge_transforms_node = Node(Merge(2), iterfield='in2', name='merge_transforms')
@@ -127,15 +141,16 @@ merge_transforms_node = Node(Merge(2), iterfield='in2', name='merge_transforms')
 
 #apply_transforms_node = Node(ApplyTransforms(reference_image=template, interpolation='BSpline'), iterables=, name='apply_transform')
 
-merge_input_files_node = Node(Merge(2), iterfield='in2', name='merge_input_files')
+#merge_input_files_node = Node(Merge(2), iterfield='in2', name='merge_input_files')
+merge_input_files_node = Node(Merge(3), iterfield=['in2', 'in3'], name='merge_input_files')
 map_apply_node = MapNode(interface=ApplyTransforms(reference_image=template, interpolation='BSpline'), iterfield=['input_image'], name='map_apply_node')
 
 transform_images = Workflow(name='cpac_epi_reg',base_dir=args['results_dir'])# base_dir='/projects/abeetem/results/cpac_epi_reg')
 
-transform_images.connect([(input_node, generate_transforms_node, [('mean', 'input_image')])])
+transform_images.connect([(data_grabber_node, generate_transforms_node, [('mean', 'input_image')])])
 #TODO: the input images are selected by the input node, but the
 
-transform_images.connect([(input_node, merge_input_files_node, [('reho', 'in1'), ('res', 'in2')])])
+transform_images.connect([(data_grabber_node, merge_input_files_node, [('reho', 'in1'), ('res_filt', 'in2'), ('mean', 'in3')])])
 # transform_images.connect([(merge_input_files_node, apply_transforms_node, [('out', 'input_image')])])
 transform_images.connect([(merge_input_files_node, map_apply_node, [('out', 'input_image')])])
 
@@ -146,11 +161,22 @@ transform_images.connect([(generate_transforms_node, merge_transforms_node, [('a
 #transform_images.connect(merge_transforms_node, 'out', apply_transforms_node, 'transforms')
 transform_images.connect(merge_transforms_node, 'out', map_apply_node, 'transforms')
 
-transform_images.write_graph(graph2use='orig', dotfilename='./cpac_epi_reg_orig', format='svg')
-transform_images.write_graph(graph2use='colored', dotfilename='./cpac_epi_reg_colored', format='svg')
-transform_images.write_graph(graph2use='exec', dotfilename='./cpac_epi_reg_exec', format='svg')
+if False:
+    data_sink_node = Node(nio.DataSink( base_directory='data_sink'), name='data_sink')
+    #data_sink_node.inputs.base_directory = args['results_dir']
+    transform_images.connect(generate_transforms_node, 'output_file', data_sink_node, 'data_sink2')
+    #generate_transforms_node.save()
+    try:
+        generate_transforms_node.get_output('output_file')
+    except:
+        print('didnt work')
 
-transform_images.run('MultiProc', plugin_args={'n_procs':30})
+if True:
+    transform_images.write_graph(graph2use='orig', dotfilename='./cpac_epi_reg_orig', format='svg')
+    transform_images.write_graph(graph2use='colored', dotfilename='./cpac_epi_reg_colored', format='svg')
+    transform_images.write_graph(graph2use='exec', dotfilename='./cpac_epi_reg_exec', format='svg')
+
+transform_images.run('MultiProc', plugin_args={'n_procs':20})
 
 """
 Input mean image: 
@@ -182,8 +208,6 @@ transform_images.write_graph(graph2use='colored', dotfilename='./test_colored', 
 transform_images.run()
 
 '''
-
-
 
 
 

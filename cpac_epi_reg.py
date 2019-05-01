17b9754cc60574443eb0e5d0c9ba60441586109d
ajb17 <anthony.beetem@gmail.com>
2019-05-01 15:45:29 -0400

    Cleaned up the code, removed unnecessary code and comments

--- cpac_epi_reg.py
+++ cpac_epi_reg.py
@@ -1,6 +1,3 @@
-# import sklearn
-# import nilearn
-
 import nipype
 import os
 from os.path import abspath
@@ -9,24 +6,13 @@ from nipype import Workflow, Node, MapNode, Function
 
 # import matplotlib.pyplot as plt
 
-from nipype.interfaces.fsl import MCFLIRT, maths, BET, FLIRT
-from nipype.interfaces.fsl.maths import MeanImage, IsotropicSmooth
 from nipype.interfaces.utility import Merge
-from nipype.interfaces.afni import SkullStrip
 from nipype.interfaces.ants import WarpImageMultiTransform, Registration, legacy, ApplyTransforms
 import argparse
-import nibabel as nib
-from scipy import stats
 
 import numpy as np
-# from nipype.scripts.crash_files import display_crash_file
 import nipype.interfaces.io as nio
-from nipype.interfaces.io import BIDSDataGrabber
-from bids import BIDSLayout
 
-
-#from pipeline_plus import get_transforms
-#import itertools
 from nipype import DataGrabber
 
 ap = argparse.ArgumentParser()
@@ -45,37 +31,7 @@ args = vars(ap.parse_args())
 
 # TODO: What is the input type for images?
 # we should end up wiht a list of image paths at least
-'''
-#TODO: split up the mean image and the derivatice image findin into tow funcitons instead
-def path_generator(sub, ses=None):
-    import os
-    from os.path import abspath
-    # mean_image = None
-    # if mean_image:
-    mean_image_string =  '/projects/cgutierrez/rockland_sample_cpac/All_Outputs/{}_{}/mean_functional/_scan_rest_acq-1400/sub-{}_ses-{}_task-rest_acq-1400_bold_calc_resample_volreg_calc_tstat.nii.gz'.format(sub, ses, sub, ses)
-    mean_image = abspath(mean_image_string)
-       # mean_image = abspath("/projects/cgutierrez/rockland_sample_cpac/All_Outputs/{}/mean_functional/_scan_rest_acq/1400/sub-{}_ses-{}_task-rest_acq1400_bold_calc_resample_volreg_calc_tstat.nii.gz".format(sub, sub, ses))
-        #print mean_image
-    reho = abspath("/projects/cgutierrez/rockland_sample_cpac/All_Outputs/{}_{}/raw_reho_map/_scan_rest_acq-1400/_compcor_ncomponents_5_selector_pc10.linear1.wm0.global1.motion1.quadratic1.gm0.compcor1.csf0/_bandpass_freqs_0.01.10.0/ReHo.nii.gz").format(sub, ses)
-    res_filt = abspath("/projects/cgutierrez/rockland_sample_cpac/All_Outputs/{}_{}/alff_img/_scan_rest_acq-1400/_compcor_ncomponents_5_selector_pc10.linear1.wm0.global1.motion1.quadratic1.gm0.compcor1.csf0/_hp_0.01/_lp_0.1/residual_filtered_3dT.nii.gz").format(sub, ses)
-    # return reho if os.path.isfile(reho) else res_filt if os.path.isfile(res_filt) else "No file found"
-    # print(res_filt)
-    # print(os.path.isfile(res_filt))
-    return mean_image if os.path.isfile(mean_image) else None, res_filt if os.path.isfile(res_filt) else None, reho if os.path.isfile(reho) else None
 
-def mean_path(sub, ses=None):
-    mean_image = abspath(
-        '/projects/cgutierrez/rockland_sample_cpac/All_Outputs/{}_{}/mean_functional/_scan_rest_acq-1400/sub-{}_ses-{}_task-rest_acq-1400_bold_calc_resample_volreg_calc_tstat.nii.gz'.format(
-            sub, ses, sub, ses))
-    return mean_image
-
-
-def reho_path(sub, ses):
-    return abspath("/projects/cgutierrez/rockland_sample_cpac/All_Outputs/{}_{}/raw_reho_map/_scan_rest_acq-1400/_compcor_ncomponents_5_selector_pc10.linear1.wm0.global1.motion1.quadratic1.gm0.compcor1.csf0/_bandpass_freqs_0.01.10.0/ReHo.nii.gz".format(sub, ses))
-def res_filt_path(sub, ses):
-    return abspath("/projects/cgutierrez/rockland_sample_cpac/All_Outputs/{}_{}/alff_img/_scan_rest_acq-1400/_compcor_ncomponents_5_selector_pc10.linear1.wm0.global1.motion1.quadratic1.gm0.compcor1.csf0/_hp_0.01/_lp_0.1/residual_filtered_3dT.nii.gz".format(sub[1:], ses))
-    #TODO: should i thorw an error if no file is found, or is this an expected thing?
-'''
 # This creates the templates and template arguments that are accepted by Nipype's data grabber
 def parse_template_entry(entry):
     key = entry[:min(entry.find(' '), entry.find('='))]
@@ -96,26 +52,10 @@ for i in open(args['derivatives'], 'r'):
     ars[unicode(key)] = [[arg.rstrip('\n').lstrip(' ') for arg in targs if arg is not '']]
     #TODO: this would probably be a little more efficient to do it as we create the list if possible
 
-'''
-if args['starter'] is not None:
-    start_images = [args['starter']]
-elif args['sub_list'] is not None and args['ses_list'] is not None:
-    start_images = [path_generator(sub, ses) for sub in args['sub_list'] for ses in args['ses_list']]
-'''
+
 
 template = abspath(args['template'])
-'''
-if args['images'] is not None:
-    image_paths = args['images']
-elif args['image_path_source'] is not None:
 
-    image_paths = args['image_path_source']
-elif args['sub_list'] is not None and args['ses_list'] is not None:
-    image_paths_lists = [path_generator(sub, ses) for sub in args['sub_list'] for ses in args['ses_list']]
-    image_paths = [path for list in image_paths_lists for path in list]
-else:
-    raise Exception("Should specify images as either a list of image paths, or an iterable source of image paths, such as a textfile, or specify a list of subjects and sessions and directory to search through")
-'''
 
 if args['sub_list'] is not None:
     if args['ses_list'] is None:
@@ -124,36 +64,20 @@ if args['sub_list'] is not None:
     ses_list = args['ses_list']
 sub_list = [sub.rstrip('\n') for sub in open(args['sub_list'], 'r')]
 ses_list = [ses.rstrip('\n') for ses in open(args['ses_list'], 'r')]
-'''
-sub_ses_dict = None
-if args['sub_list'] is not None and args['ses_list'] is not None:
-    #sub_ses_dict = {"{}_{}".format(args[sub], args[ses]):path_generator(sub, ses) for sub in args['sub_list'] for ses in args['ses_list']}
-    sub_ses_dict = {"{}_{}".format(sub.rstrip('\n'), ses.rstrip('\n')): path_generator(sub.rstrip('\n'), ses.rstrip('\n')) for sub in open(args['sub_list'], 'r') for ses in open(args['ses_list'], 'r')}
-    #sub_ses_dict = {"{}_{}".format(sub.rstrip('\n'), ses.rstrip('\n')): [reho_path(sub, ses)] + [res_filt_path(sub, ses)] for sub in args['sub_list'] for ses in args['ses_list']}
-print(sub_ses_dict)
-'''
+
 #input_node = Node(Function(output_names=['mean', 'res', 'reho'], function=path_generator), iterables=[('sub', sub_list), ('ses', ses_list)], name='input_node')
 
 def print_inputs(mean, res, reho):
     print('hello', mean, res, reho)
 print_node = Node(Function(function=print_inputs), name='print_inputs')
-#
-# testflow = Workflow(name='test', base_dir='/projects/abeetem/results/cpac_epi_reg')
-# testflow.connect([(input_node, print_node, [('mean', 'mean'), ('res','res'),('reho', 'reho')])])
-# # testflow.connect([(input_node, print_node, [('outputs.mean', 'inputs.mean'), ('outputs.res','inputs.res'),('outputs.reho', 'inputs.reho')])])
-# testflow.run()
 
 
 data_grabber_node = Node(DataGrabber(base_directory=args['directory'], sort_filelist=True, raise_on_empty=False, infields=['sub', 'ses'], outfields=['mean', 'reho', 'res_filt']), name='data_grabber')
 data_grabber_node.inputs.template = '*'
 data_grabber_node.inputs.raise_on_empty = True
 data_grabber_node.inputs.drop_blank_outputs = True
-data_grabber_node.inputs.field_template = temps#dict(mean="/projects/cgutierrez/rockland_sample_cpac/All_Outputs/%s_%s/mean_functional/_scan_rest_acq-1400/sub-%s_ses-%s_task-rest_acq-1400_bold_calc_resample_volreg_calc_tstat.nii.gz",
-                                          #     reho="/projects/cgutierrez/rockland_sample_cpac/All_Outputs/%s_%s/raw_reho_map/_scan_rest_acq-1400/_compcor_ncomponents_5_selector_pc10.linear1.wm0.global1.motion1.quadratic1.gm0.compcor1.csf0/_bandpass_freqs_0.01.10.0/ReHo.nii.gz",
-                                          #     res_filt="/projects/cgutierrez/rockland_sample_cpac/All_Outputs/%s_%s/alff_img/_scan_rest_acq-1400/_compcor_ncomponents_5_selector_pc10.linear1.wm0.global1.motion1.quadratic1.gm0.compcor1.csf0/_hp_0.01/_lp_0.1/residual_filtered_3dT.nii.gz")
-data_grabber_node.inputs.template_args = ars#dict(mean=[['sub', 'ses', 'sub', 'ses']],
-                                         #     reho=[['sub', 'ses']],
-                                         #     res_filt=[['sub', 'ses']])
+data_grabber_node.inputs.field_template = temps
+data_grabber_node.inputs.template_args = ars
 data_grabber_node.iterables = [('sub', sub_list), ('ses', ses_list)]
 
 
@@ -189,17 +113,10 @@ transform_images.connect([(generate_transforms_node, merge_transforms_node, [('a
 transform_images.connect(merge_transforms_node, 'out', map_apply_node, 'transforms')
 
 if True:
-    #for some reason, trying to add a datasink to save the result of the genwarpfield node causes the porgram to freeze,
-    # and it also causes seems to only create one data sink per run, not per subject as it should
     data_sink_node = Node(nio.DataSink( base_directory='/projects/abeetem/results/cpac_epi_reg6/data_sink'), name='data_sink')
-    #data_sink_node.inputs.base_directory = args['results_dir']
     transform_images.connect(generate_transforms_node, 'output_file', data_sink_node, 'output_file')
     transform_images.connect(map_apply_node, 'output_image', data_sink_node, 'output_image')
-    #generate_transforms_node.save()
-    # try:
-    #     generate_transforms_node.get_output('output_file')
-    # except:
-    #     print('didnt work')
+
 
 if True:
     transform_images.write_graph(graph2use='orig', dotfilename='./cpac_epi_reg_orig', format='svg')
@@ -217,40 +134,3 @@ apply transforms to these:
 
 /projects/cgutierrez/rockland_sample_cpac/All_Outputs/10936246_DS2/raw_reho_map/_scan_rest_acq-1400/_compcor_ncomponents_5_selector_pc10.linear1.wm0.global1.motion1.quadratic1.gm0.compcor1.csf0/_bandpass_freqs_0.01.10.0/ReHo.nii.gz
 """
-
-'''
-
-
-#generate_transforms_node = Node(legacy.GenWarpFields(input_image=start_image, reference_image=template), name='generate_transforms')
-generate_transforms_node = Node(legacy.GenWarpFieldsi(reference_image=template), iterables=('input_image', start_images), name='generate_transforms')
-
-merge_transforms_node = Node(Merge(2), iterfield='in2', name='merge_transforms')
-
-apply_transforms_node = Node(ApplyTransforms(reference_image=template, interpolation='BSpline'), iterables=('input_image', image_paths), name='apply_transform')
-
-
-transform_images = Workflow(name='cpac_epi_reg',base_dir = args['results_dir']# base_dir='/projects/abeetem/results/cpac_epi_reg')
-transform_images.connect([(generate_transforms_node, merge_transforms_node, [('affine_transformation', 'in2'), ('warp_field', 'in1')])])
-transform_images.connect(merge_transforms_node, 'out', apply_transforms_node, 'transforms')
-
-transform_images.write_graph(graph2use='orig', dotfilename='./test_orig', format='svg')
-transform_images.write_graph(graph2use='colored', dotfilename='./test_colored', format='svg')
-transform_images.run()
-
-'''
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
